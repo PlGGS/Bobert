49ee2cc628b01581c7e5b6451a7be4e390f20a36
PlGGS <picklepig7@comcast.net>
2017-01-19 11:31:04 -0600

    Create application and import Discord.Net API

---
+++ Bobert/Bobert/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.Audio;
+
+namespace Bobert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
