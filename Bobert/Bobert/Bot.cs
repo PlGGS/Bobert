7b69732a215b6413b6165c563ce3f65a880ec74e
PlGGS <bbor0422@gmail.com>
2017-01-22 19:40:06 -0600

    Update commands Working on audio playback

--- Bobert/Bobert/Bot.cs
+++ Bobert/Bobert/Bot.cs
@@ -2,6 +2,9 @@
 using Discord;
 using Discord.Commands;
 using Discord.Audio;
+using YoutubeSearch;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bobert
 {
@@ -9,8 +12,11 @@ namespace Bobert
     {
         DiscordClient client;
         CommandService cmds;
-        char prefix = '\u005c';
+        IAudioClient vClient;
 
+        VideoSearch items = new VideoSearch();
+        List<VideoInformation> list = new List<VideoInformation>();
+        
         public Bot()
         {
             client = new DiscordClient(input =>
@@ -21,15 +27,61 @@ namespace Bobert
             
             client.UsingCommands(input =>
             {
-                input.PrefixChar = prefix;
+                input.PrefixChar = '\\';
                 input.AllowMentionPrefix = true;
             });
-            
+
             cmds = client.GetService<CommandService>();
 
+            client.UsingAudio(x =>
+            {
+                x.Mode = AudioMode.Outgoing;
+            });
+            
             cmds.CreateCommand("herro").Do(async (e) =>
             {
                 await e.Channel.SendMessage("World!");
+            });
+            
+            cmds.CreateGroup("play", cgb =>
+            {
+                string videoQuery = "";
+
+                cgb.CreateCommand("youtube")
+                        .Alias(new string[] { "yt", "y" })
+                        .Description("Plays a YouTube video's audio.")
+                        .Parameter("videoName", ParameterType.Required)
+                        .Do(async e =>
+                        {
+                            foreach (char element in e.GetArg("videoName"))
+                            {
+                                if (element == '_')
+                                {
+                                    videoQuery = e.GetArg("videoName").ToString().Replace('_', ' ');
+                                }
+                            }
+
+                            if (!videoQuery.Contains(" "))
+                            {
+                                videoQuery = e.GetArg("videoName");
+                            }
+                            
+                            await e.Channel.SendMessage($"{e.User.Name} played the YouTube video: {videoQuery}");
+
+                            var voiceChannel = client.FindServers(e.User.VoiceChannel.ToString()).FirstOrDefault().VoiceChannels.FirstOrDefault();
+                            var _vClient = client.GetService<AudioService>().Join(voiceChannel);
+
+                            //play audio n' shit
+                        });
+
+                cgb.CreateCommand("spotify")
+                        .Alias(new string[] { "sp", "s" })
+                        .Description("Plays a song from Spotify.")
+                        .Parameter("GreetedPerson", ParameterType.Required)
+                        .Do(async e =>
+                        {
+                            await e.Channel.SendMessage($"{e.User.Name} says goodbye to {e.GetArg("GreetedPerson")}");
+                        });
             });
 
             client.ExecuteAndWait(async () => { await client.Connect("MjcxNjk3OTA1NzIxNTQwNjA5.C2KYcA.wDKEh-OWHTw0XazldDs_dYniMSA", TokenType.Bot); });
