ea707825d7287d37a03e2669dbb75c4d0e15d3c7
PlGGS <bbor0422@gmail.com>
2017-02-25 10:21:51 -0600

    Fixed a bug that crashed the bot if a user asked to see files and then tried to stop a song

--- Bobert/Bobert/Bot.cs
+++ Bobert/Bobert/Bot.cs
@@ -36,7 +36,7 @@ namespace Bobert
                 input.LogHandler = Log;
                 SetArrayValues();
             });
-            
+
             client.UsingCommands(input =>
             {
                 input.PrefixChar = '/';
@@ -50,7 +50,7 @@ namespace Bobert
             });
 
             cmds = client.GetService<CommandService>();
-            
+
             cmds.CreateCommand("listFiles").Do(async (e) =>
             {
                 await e.Channel.SendMessage("To add more files, go to https://www.dropbox.com/sh/8vy5iz7ndsgcnpl/AAA6yI_TcR_swccegTeTpcqfa?dl=0, and drop in your own (MP3 and WAV files only, NO spaces in the file names)");
@@ -141,7 +141,7 @@ namespace Bobert
                                 {
                                     e.Channel.SendMessage("Please join a voice channel before attempting to play audio");
                                 }
-                                
+
                                 if (fileTypeIndex != -1)
                                 {
                                     audioPlaying = true;
@@ -154,7 +154,7 @@ namespace Bobert
                                 {
                                     audioPlaying = true;
                                     currentAudio = e.GetArg("fileName");
-                                    
+
                                     e.Channel.SendMessage($"{e.User.Name} played a random audio file ({fileNames[rnd]})");
                                     SendAudio(audioPath + fileNames[rnd] + fileTypes[rnd]);
                                 }
@@ -165,7 +165,6 @@ namespace Bobert
 
                                 //SendAudio(videoSearchItems.SearchQuery(e.GetArg("videoName"), 1)[0].Url); //OLD WAY OF FINDING YOUTUBE VIDEOS
                                 //TODO possibly remove youtubesearch.dll from references if it's just not gonna happen
-                                //TODO fix 'random' playback of han
                             }
                         });
 
@@ -186,7 +185,12 @@ namespace Bobert
                     await e.Channel.SendFile($"{e.User.Name} tried to stop current audio. Nothing was playing...");
                 }
             });
-            
+
+            ConnectBot();
+        }
+
+        private void ConnectBot()
+        {
             client.ExecuteAndWait(async () => { await client.Connect("MjcxNjk3OTA1NzIxNTQwNjA5.C2KYcA.wDKEh-OWHTw0XazldDs_dYniMSA", TokenType.Bot); });
         }
 
@@ -198,9 +202,13 @@ namespace Bobert
 
             for (int i = 0; i < Directory.GetFiles(audioPath).Length; i++)
             {
-                fileNames[i] = audioFiles[i].Substring(audioPath.Length, audioFiles[i].Substring(audioPath.Length - 1).Length - 1);
-                fileTypes[i] = audioFiles[i].Substring(audioPath.Length + fileNames[i].Length - 4, 4);
-                fileNames[i] = fileNames[i].Substring(0, fileNames[i].Length - 4);
+                if (audioFiles[i].Substring(audioFiles[i].Length - 4, 4) != ".wav" ||
+                    audioFiles[i].Substring(audioFiles[i].Length - 4, 4) != ".mp3")
+                {
+                    fileNames[i] = audioFiles[i].Substring(audioPath.Length, audioFiles[i].Substring(audioPath.Length - 1).Length - 1);
+                    fileTypes[i] = audioFiles[i].Substring(audioPath.Length + fileNames[i].Length - 4, 4);
+                    fileNames[i] = fileNames[i].Substring(0, fileNames[i].Length - 4);
+                }
             }
         }
 
@@ -211,37 +219,45 @@ namespace Bobert
 
         public async void SendAudio(string pathOrUrl)
         {
-            //TODO make sure this works as a public variable
-            vClient = await client.GetService<AudioService>().Join(client.FindServers(serverName).FirstOrDefault().FindChannels(channelName, ChannelType.Voice, true).FirstOrDefault());
-
-            procFFMPEG = Process.Start(new ProcessStartInfo
-            { // FFmpeg requires us to spawn a process and hook into its stdout, so we will create a Process
-                FileName = "ffmpeg",
-                Arguments = $"-i {pathOrUrl} " + // Here we provide a list of arguments to feed into FFmpeg. -i means the location of the file/URL it will read from
-                            "-f s16le -ar 48000 -ac 2 pipe:1", // Next, we tell it to output 16-bit 48000Hz PCM, over 2 channels, to stdout.
-                UseShellExecute = false,
-                RedirectStandardOutput = true // Capture the stdout of the process
-            });
-
-            procFFMPEG.PriorityBoostEnabled = true;
-            System.Threading.Thread.Sleep(2000); // Sleep for a few seconds to FFmpeg can start processing data.
-
-            int blockSize = 3840; // The size of bytes to read per frame; 1920 for mono
-            byte[] buffer = new byte[blockSize];
-            int byteCount;
-
-            while (audioPlaying) // TODO check to see if user stops the audio playback
+            try
             {
-                byteCount = procFFMPEG.StandardOutput.BaseStream // Access the underlying MemoryStream from the stdout of FFmpeg
-                        .Read(buffer, 0, blockSize); // Read stdout into the buffer
+                //TODO make sure this works as a public variable
+                vClient = await client.GetService<AudioService>().Join(client.FindServers(serverName).FirstOrDefault().FindChannels(channelName, ChannelType.Voice, true).FirstOrDefault());
 
-                if (byteCount == 0) // FFmpeg did not output anything
-                    break; // Break out of the while(true) loop, since there was nothing to read.
+                procFFMPEG = Process.Start(new ProcessStartInfo
+                { // FFmpeg requires us to spawn a process and hook into its stdout, so we will create a Process
+                    FileName = "ffmpeg",
+                    Arguments = $"-i {pathOrUrl} " + // Here we provide a list of arguments to feed into FFmpeg. -i means the location of the file/URL it will read from
+                                "-f s16le -ar 48000 -ac 2 pipe:1", // Next, we tell it to output 16-bit 48000Hz PCM, over 2 channels, to stdout.
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true // Capture the stdout of the process
+                });
 
-                vClient.Send(buffer, 0, byteCount); // Send our data to Discord
+                procFFMPEG.PriorityBoostEnabled = true;
+                System.Threading.Thread.Sleep(2000); // Sleep for a few seconds to FFmpeg can start processing data.
+
+                int blockSize = 3840; // The size of bytes to read per frame; 1920 for mono
+                byte[] buffer = new byte[blockSize];
+                int byteCount;
+
+                while (audioPlaying) // TODO check to see if user stops the audio playback
+                {
+                    byteCount = procFFMPEG.StandardOutput.BaseStream // Access the underlying MemoryStream from the stdout of FFmpeg
+                            .Read(buffer, 0, blockSize); // Read stdout into the buffer
+
+                    if (byteCount == 0) // FFmpeg did not output anything
+                        break; // Break out of the while(true) loop, since there was nothing to read.
+
+                    vClient.Send(buffer, 0, byteCount); // Send our data to Discord
+                }
+                vClient.Wait(); // Wait for the Voice Client to finish sending data, as ffMPEG may have already finished buffering out a song, and it is unsafe to return now.
+                await vClient.Disconnect();
             }
-            vClient.Wait(); // Wait for the Voice Client to finish sending data, as ffMPEG may have already finished buffering out a song, and it is unsafe to return now.
-            await vClient.Disconnect();
+            catch (OperationCanceledException)
+            {
+                ConnectBot();
+                SendAudio(pathOrUrl);
+            }
         }
     }
 }
